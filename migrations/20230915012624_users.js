8c51a73af171475bca98b8ebe6ac05ccac8ca00f
Jeffry Rojas <jeffrysnneyder.rr@gmail.com>
2023-09-14 20:33:17 -0500

    user and skills tables

--- migrations/20230915012624_users.js
+++ migrations/20230915012624_users.js
@@ -6,7 +6,7 @@ exports.up = function(knex) {
     return knex.schema.hasTable('users').then((exists)=>{
         if (!exists) {
             return knex.schema.createTable("users", function(table){
-                table.increments("is").primary();
+                table.increments("id").primary();
                 table.string("first_name").notNullable();
                 table.string("last_name").notNullable();
                 table.integer("fee").notNullable();
