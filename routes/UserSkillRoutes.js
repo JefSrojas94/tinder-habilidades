5751bc75fd0488c5e21ec1a258dd0941f936e3e3
Jeffry Rojas <jeffrysnneyder.rr@gmail.com>
2023-09-14 19:33:16 -0500

    changes in folder Routes and Controllers

--- routes/UserSkillRoutes.js
+++ routes/UserSkillRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const {UserSkillController} = require('../controllers/UserSkillController');
 
 router.get('/',UserSkillController.getUserSkills);
 
