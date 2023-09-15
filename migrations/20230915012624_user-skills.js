a40a4af4d0c3235e425fcda08ded46c84f9a61b9
Jeffry Rojas <jeffrysnneyder.rr@gmail.com>
2023-09-14 20:56:00 -0500

    user-skills tables

--- migrations/20230915012624_user-skills.js
+++ migrations/20230915012624_user-skills.js
@@ -3,7 +3,15 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema.hasTable('user-skills').then((exists)=>{
+        if (!exists) {
+            return knex.schema.createTable("user-skills", function(table){
+                table.increments('id').primary();
+                table.integer('id_user').references('id').inTable('users');
+                table.integer('id_skill').references('id').inTable('skills');             
+            });
+        }
+    });
 };
 
 /**
